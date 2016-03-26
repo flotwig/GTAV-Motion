0f3d12ee3f35404fafb9f1139adaafc2bfdceb93
Zach Bloomquist <zrbq@live.com>
2016-03-26 03:59:04 -0400

    Restructure

--- GTAV-Motion/script.cpp
+++ GTAV-Motion/script.cpp
@@ -10,28 +10,9 @@ Ped player;
 Vehicle playerVehicle;
 float steerBias = 0.0;
 
-void main() {	
-	while (true) {
-		/**
-			Call functions in this script which steer the vehicle more left or
-			more right. Using this primitive:
-			http://www.dev-c.com/nativedb/func/info/42a8ec77d5150cbe
-			VEHICLE::SET_VEHICLE_STEER_BIAS(Vehicle vehicle, float value)
-			value is between -1.0 and 1.0, with -1.0 being full left and 1.0 
-			being full right.
-		*/
-		/* somehow Harsha's code needs to end up calling turn_right or turn_left.
-			in this loop we should poll frames from the motion detector and see if
-			a turn is necessary. turn_right/_left will update steerBias which is
-			then sent to the game as below. */
-		/* at the same time, we need to poll Dylan's controller to see if accel or
-			brakes should be applied in a similar fashion. (accelerate(), brake()) */
-		// TODO: implement motion detection here
-		// TODO: implement buttons here
-		// TODO: figure out which natives control acceleration/braking
-		VEHICLE::SET_VEHICLE_STEER_BIAS(playerVehicle, steerBias);
-		WAIT(0);
-	}
+void update_steer_bias() {
+	// TODO: retrieve latest steer bias from game and update steerBias
+	return;
 }
 
 void turn_right() {
@@ -46,14 +27,34 @@ void turn_left() {
 	steerBias = steerBias < -1.0 ? -1.0 : steerBias;
 }
 
-void update_steer_bias() {
-	// TODO: retrieve latest steer bias from game and update steerBias
-	return;
+void main() {	
+	while (true) {
+		float initialSteerBias = steerBias;
+		/*
+			GTAV native functions!: http://www.dev-c.com/nativedb/func/info/42a8ec77d5150cbe
+		*/
+		/* somehow Harsha's code needs to end up calling turn_right or turn_left.
+			in this loop we should poll frames from the motion detector and see if
+			a turn is necessary. turn_right/_left will update steerBias which is
+			then sent to the game as below. */
+		/* at the same time, we need to poll Dylan's controller to see if accel or
+			brakes should be applied in a similar fashion. (accelerate(), brake()) */
+		// TODO: implement motion detection here
+		// TODO: implement buttons here
+		// TODO: figure out which natives control acceleration/braking
+		// If something has affected the steerBias during this iteration...
+		if (initialSteerBias != steerBias) {
+			// ...set the steer bias of the vehicle the current player ped is currently in.
+			VEHICLE::SET_VEHICLE_STEER_BIAS(
+				PED::GET_VEHICLE_PED_IS_IN(PLAYER::PLAYER_PED_ID(), 0),
+				steerBias
+			);
+		}
+		WAIT(0);
+	}
 }
 
 void ScriptMain() {
-	player = PLAYER::PLAYER_PED_ID();
-	playerVehicle = PED::GET_VEHICLE_PED_IS_IN(player, 0); // 0 = CurrentVehicle, 1 = LastVehicle
 	// note: after looking at it for long enough, "player" ceases to look like a word
 	srand(GetTickCount());
 	main();
