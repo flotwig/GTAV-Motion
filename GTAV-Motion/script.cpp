6a1dc5de3259bec5f68bbada4e4c29320855d5b0
Zach Bloomquist <zrbq@live.com>
2016-03-26 08:17:43 -0400

    no sleep

--- GTAV-Motion/script.cpp
+++ GTAV-Motion/script.cpp
@@ -1,12 +1,11 @@
 #include "script.h"
+#include "Leap.h"
 
 #include <string>
 #include <ctime>
 
 #pragma warning(disable : 4244 4305) // double <-> float conversions
 
-Ped player;
-Vehicle playerVehicle;
 float steerBias = 0.0;
 
 void update_steer_bias() {
@@ -26,30 +25,52 @@ void turn_left() {
 	steerBias = steerBias < -1.0 ? -1.0 : steerBias;
 }
 
+void accelerate() {
+	// TODO: implement
+	return;
+}
+
+void brake() {
+	// TODO: implement
+	return;
+}
+
+void loop() {
+	float initialSteerBias = steerBias;
+	/*
+	GTAV native functions!: http://www.dev-c.com/nativedb/func/info/42a8ec77d5150cbe
+	*/
+	/* somehow Harsha's code needs to end up calling turn_right or turn_left.
+	in this loop we should poll frames from the motion detector and see if
+	a turn is necessary. turn_right/_left will update steerBias which is
+	then sent to the game as below. */
+	/* at the same time, we need to poll Dylan's controller to see if accel or
+	brakes should be applied in a similar fashion. (accelerate(), brake()) */
+	// TODO: implement motion detection here and call turn_right/_left
+	// TODO: implement buttons here and call accelerate/brake
+	/* How to tell if someone is turning? Could use Hand.grabStrength() to see
+	 * if player is gripping the wheel and could track translation of the hand
+	 * between frames to see if significant movement has been made. */
+	// If something has affected the steerBias during this iteration...
+	if (initialSteerBias != steerBias) {
+		// ...set the steer bias of the vehicle the current player ped is currently in.
+		VEHICLE::SET_VEHICLE_STEER_BIAS(
+			PED::GET_VEHICLE_PED_IS_IN(PLAYER::PLAYER_PED_ID(), 0),
+			steerBias
+			);
+	}
+}
+
 void main() {	
 	while (true) {
-		float initialSteerBias = steerBias;
-		/*
-			GTAV native functions!: http://www.dev-c.com/nativedb/func/info/42a8ec77d5150cbe
-		*/
-		/* somehow Harsha's code needs to end up calling turn_right or turn_left.
-			in this loop we should poll frames from the motion detector and see if
-			a turn is necessary. turn_right/_left will update steerBias which is
-			then sent to the game as below. */
-		/* at the same time, we need to poll Dylan's controller to see if accel or
-			brakes should be applied in a similar fashion. (accelerate(), brake()) */
-		// TODO: implement motion detection here
-		// TODO: implement buttons here
-		// TODO: figure out which natives control acceleration/braking
-		// If something has affected the steerBias during this iteration...
-		if (initialSteerBias != steerBias) {
-			// ...set the steer bias of the vehicle the current player ped is currently in.
-			VEHICLE::SET_VEHICLE_STEER_BIAS(
-				PED::GET_VEHICLE_PED_IS_IN(PLAYER::PLAYER_PED_ID(), 0),
-				steerBias
-			);
-		}
 		WAIT(0);
+		// check to make sure player exists and it makes sense for them to be driving
+		if (!ENTITY::DOES_ENTITY_EXIST(PLAYER::PLAYER_PED_ID()) ||
+			!PLAYER::IS_PLAYER_CONTROL_ON(PLAYER::PLAYER_ID()) ||
+			ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID()) ||
+			PLAYER::IS_PLAYER_BEING_ARRESTED(PLAYER::PLAYER_ID(), TRUE))
+			continue;
+		loop();
 	}
 }
 
